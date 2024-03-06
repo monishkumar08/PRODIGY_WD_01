e546c51756f832532632b8e75cd3052d35cdae04
Monish Kumar R <126189485+monishkumar08@users.noreply.github.com>
2024-03-06 22:49:39 +0530

    Delete TASK-1 directory

--- TASK-1/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    const menu = document.getElementById('menu');
-    if (window.scrollY > 50) {
-        menu.classList.add('scrolled');
-    } else {
-        menu.classList.remove('scrolled');
-    }
-});
